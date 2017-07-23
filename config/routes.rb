f5462c57967e3dd10813784870b5ca0aaac46d86
Matilde Rosa <matilderosa3@hotmail.com>
2017-07-23 15:53:29 +0100

    Added stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+
+  get 'orders/show'
+
+  get 'orders/new'
+
+  get 'orders/create'
+
+  get 'orders/edit'
+
+  get 'orders/update'
+
+  get 'orders/destroy'
+
   devise_for :users
   root to: 'pages#home'
 
   get 'about',       to: 'pages#about'
   get 'contacts',     to: 'pages#contacts'
   get 'home2',     to: 'pages#home2'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
