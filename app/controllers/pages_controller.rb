1a1629e13a753a6cec55a6e8a1e4209ffb4899dd
Matilde Rosa <matilderosa3@hotmail.com>
2017-07-29 19:20:37 +0100

    added views and controllers and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,7 @@ class PagesController < ApplicationController
 
   def home2
   end
+
+  def gallery
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'contacts',     to: 'pages#contacts'
   get 'home2',     to: 'pages#home2'
   resources :orders
+  get 'gallery', to: 'pages#gallery'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
