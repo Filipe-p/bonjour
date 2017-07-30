88f3c4ff329e04be5c49547d7ae4fa3db150a811
filipe paiva <filipe.p@soltroiavillas.com>
2017-07-30 19:09:40 +0100

    created the seed file to make cakes in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729181422) do
+ActiveRecord::Schema.define(version: 20170730171344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170729181422) do
     t.bigint "filling_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "size"
     t.index ["decoration_id"], name: "index_cakes_on_decoration_id"
     t.index ["dough_id"], name: "index_cakes_on_dough_id"
     t.index ["filling_id"], name: "index_cakes_on_filling_id"
