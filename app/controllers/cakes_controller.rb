f8b95d93392f4f901504f5dad6f833f33162e8b3
Matilde Rosa <null>
2017-08-15 00:11:59 +0100

    Added ajax

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -19,44 +19,55 @@ class CakesController < ApplicationController
 
     if @order.save
       session[:order_id] = @order.id
-
-      @cake = Cake.new(cake_params)
-      @cake.order = @order
-
-      if @cake.save
-        redirect_to new_cake_dough_filling_path(@cake, @cake.dough)
+      if params[:cake].blank?
+        flash[:alert] = "Seleccione uma opção"
+        @cake = Cake.new
+        @doughs = Dough.all
+        render "cakes/new"
       else
-        render :new
+        @cake = Cake.new(cake_params)
+        @cake.order = @order
+        if @cake.save
+          respond_to do |format|
+            format.html { redirect_to new_cake_dough_filling_path(@cake, @cake.dough) }
+            format.js  # <-- will render `app/views/cakes/create.js.erb`
+          end
+        else
+          respond_to do |format|
+            format.html { render :new }
+            format.js
+          end
+        end
       end
-
     else
       render :new
     end
   end
 
-  def edit
-    if @cake.update(cake_params)
-      redirect_to cake_dough_filling_path(@cake, @cake.dough)
-    else
-      render :edit
-    end
+def edit
+  if @cake.update(cake_params)
+    redirect_to cake_dough_filling_path(@cake, @cake.dough)
+  else
+    render :edit
   end
+end
 
-  def update
-  end
+def update
+end
 
-  def delete
-  end
+def delete
+end
 
-  private
+private
 
-  def cake_params
-    cake_params = params.require(:cake).permit(:dough)
-    cake_params[:dough] = Dough.find(cake_params[:dough])
-    cake_params
-  end
+def cake_params
 
-  def set_cake
-    @cake = Cake.find(params[:id])
-  end
+  cake_params = params.require(:cake).permit(:dough)
+  cake_params[:dough] = Dough.find(cake_params[:dough])
+  cake_params
+end
+
+def set_cake
+  @cake = Cake.find(params[:id])
+end
 end
