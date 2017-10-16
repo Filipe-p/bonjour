811f87070df0dd3be2a08f40b130b8839a84858a
Filipe Paiva <filipe.p@soltroiavillas.com>
2017-10-17 00:53:55 +0100

    Mailchimp working but with wrong account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001190521) do
+ActiveRecord::Schema.define(version: 20171016231732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,12 @@ ActiveRecord::Schema.define(version: 20171001190521) do
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
     t.index ["order_id"], name: "index_others_on_order_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
