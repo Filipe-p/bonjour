d800eb792e66c6b4eb858f6b686c26b8cc218dfd
Pedro Rio <prio@teachme.site>
2017-10-18 23:22:31 +0100

    Started translations and inclusion list of models and attributes

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,6 +4,148 @@ RailsAdmin.config do |config|
     redirect_to main_app.root_path unless current_user && current_user.admin
   end
 
+  config.main_app_name = "Pastelaria Bonjour"
+
+  config.excluded_models = ["OrderOther", "DoughFilling", "DecorationCategory"]
+
+  # Cake
+  # ===========================
+  config.model 'Cake' do
+    include_fields :dough, :filling, :decoration, :message, :order, :price_cents, :age, :size
+  end
+
+  # User
+  # ===========================
+  config.model 'User' do
+    include_fields :admin, :first_name, :last_name, :email, :orders
+  end
+
+  # Contact
+  # ===========================
+  config.model 'Contact' do
+    include_fields :contact_first_name, :contact_last_name, :contact_email, :contact_telephone, :message
+  end
+
+  # Category
+  # ===========================
+  config.model 'Category' do
+    include_fields :name, :description
+  end
+
+  # CustomCake
+  # ===========================
+  config.model 'CustomCake' do
+    include_fields :contact_first_name, :contact_last_name, :contact_email, :contact_telephone, :message
+  end
+
+  # Decoration
+  # ===========================
+  config.model 'Decoration' do
+    include_fields :name, :description, :photo, :minimum_size, :price_cents, :categories
+  end
+
+  # Delivery
+  # ===========================
+  config.model 'Delivery' do
+    include_fields :done, :nif, :zipcode, :payment, :cost_cents, :store_pickup, :address, :delivery_date, :delivery_slot, :contact_first_name, :contact_last_name, :contact_telephone, :contact_email, :user, :order
+  end
+
+  # Dough
+  # ===========================
+  config.model 'Dough' do
+    include_fields :name, :description, :photo, :price_cents
+  end
+
+  # FeaturedCake
+  # ===========================
+  config.model 'FeaturedCake' do
+    include_fields :name
+  end
+
+  # Filling
+  # ===========================
+  config.model 'Filling' do
+    include_fields :name, :description, :price_cents
+  end
+
+  # Order
+  # ===========================
+  config.model 'Order' do
+    include_fields :done, :user, :cakes, :others, :delivery
+  end
+
+  # Other
+  # ===========================
+  config.model 'Other' do
+    include_fields :name, :photo, :price_cents
+  end
+
+  # Subscriber
+  # ===========================
+  config.model 'Subscriber' do
+    include_fields :email
+  end
+
+
+  # Operation group
+  # ===========================
+
+  config.model 'Cake' do
+    navigation_label 'Operações'
+  end
+  config.model 'Delivery' do
+    navigation_label 'Operações'
+  end
+  config.model 'Order' do
+    navigation_label 'Operações'
+  end
+
+  # Configuration group
+  # ===========================
+
+  config.model 'Dough' do
+    navigation_label 'Configuração'
+  end
+
+  config.model 'Filling' do
+    navigation_label 'Configuração'
+  end
+
+  config.model 'Decoration' do
+    navigation_label 'Configuração'
+  end
+
+  config.model 'Category' do
+    navigation_label 'Configuração'
+  end
+
+  config.model 'Other' do
+    navigation_label 'Configuração'
+  end
+
+  # Marketing group
+  # ===========================
+
+  config.model 'User' do
+    navigation_label 'Marketing'
+  end
+
+  config.model 'Subscriber' do
+    navigation_label 'Marketing'
+  end
+
+  config.model 'Contact' do
+    navigation_label 'Marketing'
+  end
+
+  config.model 'CustomCake' do
+    navigation_label 'Marketing'
+  end
+
+  config.model 'FeaturedCake' do
+    navigation_label 'Marketing'
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
